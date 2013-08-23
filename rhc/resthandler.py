c52f51be553dd04726fecf3f7e60761ff43fb76c
bob chase <git@tympanum.org>
2013-08-23 13:41:07 -0400

    new version.
    add rest support.

---
+++ rhc/resthandler.py
@@ -0,0 +1,117 @@
+'''
+The MIT License (MIT)
+
+Copyright (c) 2013 Robert H Chase
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+import json
+import re
+
+import traceback
+
+from httphandler import HTTPHandler
+
+
+class RESTHandler(HTTPHandler):
+    '''
+        Identify and execute REST handler functions.
+
+        The RESTHandler context is a RESTMapper instance, with mappings defined
+        for each URI.
+
+        A handler function normally calls rest_result(...) to respond.
+
+        Callbacks:
+
+            on_rest_not_found() - default: 202 Not Found
+            on_rest_server_exception(exception) - default: 500 Internal Server Error + content=str(exception)
+
+        Helper:
+
+            rest_unauthorized(self) - 401 Unauthorized
+            rest_bad_request(self,content=None) - 400 Bad Request
+
+        Response:
+
+            rest_result(content, code, message)
+    '''
+
+    def rest_bad_request(self, content=None):
+        self.rest_result(content, code=400, message='Bad Request')
+
+    def rest_unauthorized(self):
+        self.rest_result(code=401, message='Unauthorized')
+
+    def on_rest_not_found(self):
+        self.rest_result(code=404, message='Not Found')
+
+    def on_rest_server_exception(self, exception):
+        self.rest_result(str(exception), code=500, message='Internal Server Error')
+
+    def on_http_data(self):
+        handler, groups = self.context.match(self.http_resource, self.http_method)
+        if handler:
+            try:
+                handler(self, *groups)
+            except Exception, e:
+                traceback.format_exc()
+                self.on_rest_server_exception(e)
+        else:
+            self.on_rest_not_found()
+
+    def rest_result(self, content=None, code=200, message='OK'):
+        args = {'code':code, 'message':message}
+        if content:
+            args['content'] = content
+        self.send_server(**args)
+
+class RESTMapper(object):
+
+    def __init__(self):
+        self.__mapping = []
+        self.setup()
+
+    def setup(self):
+        pass
+
+    def add(self, pattern, get=None, post=None, put=None, delete=None):
+        self.__mapping.append(
+            RESTMapping(pattern, get, post, put, delete)
+        )
+
+    def match(self, resource, method):
+        for mapping in self.__mapping:
+            m = mapping.pattern.match(resource)
+            if m:
+                handler = mapping.method[method.lower()]
+                if handler:
+                    return handler, m.groups()
+        return None, None
+
+class RESTMapping(object):
+
+    def __init__(self, pattern, get, post, put, delete):
+        self.pattern = re.compile(pattern)
+        self.method = {
+            'get' : get,
+            'post' : post,
+            'put' : put,
+            'delete' : delete
+        }
---
+++ test/test_resthandler.py
@@ -0,0 +1,27 @@
+import unittest
+from rhc.resthandler import RESTMapper
+
+class RESTHandlerTest(unittest.TestCase):
+
+    def setUp(self):
+        self.c = RESTMapper()
+        self.c.add('/test$', 1, 2, 3, 4)
+
+        self.c.add('/foo$', 1, 2)
+        self.c.add('/foo$', put=5)
+
+    def test_basic(self):
+        h, g = self.c.match('/test', 'GET')
+        self.assertEqual(h, 1)
+        self.assertEqual(g, ())
+
+    def test_nomatch(self):
+        h, g = self.c.match('/testt', 'GET')
+        self.assertIsNone(h)
+        self.assertIsNone(g)
+
+    def test_multiple(self):
+        h, g = self.c.match('/foo', 'post')
+        self.assertEqual(h, 2)
+        h, g = self.c.match('/foo', 'put')
+        self.assertEqual(h, 5)
