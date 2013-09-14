db817effcf7b043d2d6ff5a6c51b16ed3dc6f906
bob chase <git@tympanum.org>
2013-09-14 09:59:13 -0400

    fix bug, add comments.

--- rhc/resthandler.py
+++ rhc/resthandler.py
@@ -39,13 +39,13 @@ class RESTHandler(HTTPHandler):
 
         A handler function calls rest_send(...) to send a response.
 
-        Callback
-
-            on_rest_send(code, message, content)
-
         To send response:
 
             rest_send(content, code, message)
+
+        Callback
+
+            on_rest_send(code, message, content)
     '''
 
     def on_http_data(self):
@@ -59,7 +59,7 @@ class RESTHandler(HTTPHandler):
                 self.rest_send(str(
                     e), code=501, message='Internal Server Error')
         else:
-            self.rest_not_found()
+            self.rest_send(code=404, message='Not Found')
 
     def rest_send(self, content=None, code=200, message='OK'):
         args = {'code': code, 'message': message}
@@ -74,19 +74,62 @@ class RESTHandler(HTTPHandler):
 
 class RESTMapper(object):
 
+    '''
+        A URI-to-executable mapper that is passed as the context for a
+        RESTHandler.
+
+        The on_http_data method of the RESTHandler calls the match method
+        on this object to resolve a URI to a previously defined pattern.
+        Patterns are added with the add method.
+    '''
+
     def __init__(self):
         self.__mapping = []
-        self.setup()
-
-    def setup(self):
-        pass
 
     def add(self, pattern, get=None, post=None, put=None, delete=None):
+        '''
+            Add a mapping between a URI and a CRUD method.
+
+            The pattern is a regex string which can include groups. If
+            groups are included in the regex, they will be passed as
+            parameters to the matching method.
+
+            The match method will evaluate each mapping in the order
+            that they are added. The first match wins.
+
+            For example:
+
+                add('/foo/(\d+)/bar', get=my_func)
+
+                will match:
+
+                    GET /foo/123/bar HTTP/1.1
+
+                resulting in the call:
+
+                    my_func(123)
+
+                in this case, my_func must be defined to take the
+                parameter.
+        '''
         self.__mapping.append(
             RESTMapping(pattern, get, post, put, delete)
         )
 
     def match(self, resource, method):
+        '''
+            Match a resource + method to a RESTMapping
+
+            The resource parameter is the resource string from the
+            http call. The method parameter is the method from
+            the http call. The user shouldn't call this method, it
+            is called by the on_http_data method of the
+            RESTHandler.
+
+            Step through the mappings in the order they were defined
+            and look for a match on the regex which also has a method
+            defined.
+        '''
         for mapping in self.__mapping:
             m = mapping.pattern.match(resource)
             if m:
@@ -97,6 +140,8 @@ class RESTMapper(object):
 
 
 class RESTMapping(object):
+
+    ''' container for one mapping definition '''
 
     def __init__(self, pattern, get, post, put, delete):
         self.pattern = re.compile(pattern)
