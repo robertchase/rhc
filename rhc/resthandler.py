fdd38fc63a3150fae2736461e0212c8112250d14
bob chase <git@tympanum.org>
2013-09-14 08:10:35 -0400

    remove helper functions. change rest_result to rest_send.

--- rhc/resthandler.py
+++ rhc/resthandler.py
@@ -30,62 +30,47 @@ from httphandler import HTTPHandler
 
 
 class RESTHandler(HTTPHandler):
+
     '''
         Identify and execute REST handler functions.
 
         The RESTHandler context is a RESTMapper instance, with mappings defined
         for each URI.
 
-        A handler function normally calls rest_result(...) to respond, or one of the cover functions.
+        A handler function calls rest_send(...) to send a response.
 
         Callback
 
-            on_rest_result(code, message, content)
+            on_rest_send(code, message, content)
 
-        Helper (cover functions for rest_result):
+        To send response:
 
-            rest_not_found() - default: 202 Not Found
-            rest_unauthorized(self) - 401 Unauthorized
-            rest_bad_request(self,content=None) - 400 Bad Request
-            rest_server_exception(exception) - default: 500 Internal Server Error + content=str(exception)
-
-        Response:
-
-            rest_result(content, code, message)
+            rest_send(content, code, message)
     '''
 
-    def rest_bad_request(self, content=None):
-        self.rest_result(content, code=400, message='Bad Request')
-
-    def rest_unauthorized(self):
-        self.rest_result(code=401, message='Unauthorized')
-
-    def rest_not_found(self):
-        self.rest_result(code=404, message='Not Found')
-
-    def rest_server_exception(self, exception):
-        self.rest_result(str(exception), code=500, message='Internal Server Error')
-
     def on_http_data(self):
-        handler, groups = self.context.match(self.http_resource, self.http_method)
+        handler, groups = self.context.match(
+            self.http_resource, self.http_method)
         if handler:
             try:
                 handler(self, *groups)
             except Exception, e:
                 traceback.format_exc()
-                self.rest_server_exception(e)
+                self.rest_send(str(
+                    e), code=501, message='Internal Server Error')
         else:
             self.rest_not_found()
 
-    def rest_result(self, content=None, code=200, message='OK'):
-        args = {'code':code, 'message':message}
+    def rest_send(self, content=None, code=200, message='OK'):
+        args = {'code': code, 'message': message}
         if content:
             args['content'] = content
-        self.on_rest_result(code, message, content)
+        self.on_rest_send(code, message, content)
         self.send_server(**args)
 
-    def on_rest_result(self, code, message, content):
+    def on_rest_send(self, code, message, content):
         pass
+
 
 class RESTMapper(object):
 
@@ -110,13 +95,14 @@ class RESTMapper(object):
                     return handler, m.groups()
         return None, None
 
+
 class RESTMapping(object):
 
     def __init__(self, pattern, get, post, put, delete):
         self.pattern = re.compile(pattern)
         self.method = {
-            'get' : get,
-            'post' : post,
-            'put' : put,
-            'delete' : delete
+            'get': get,
+            'post': post,
+            'put': put,
+            'delete': delete
         }
