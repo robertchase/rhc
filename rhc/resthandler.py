b58de3dd276406c08128454098c354f6e8144fed
bob chase <bchase@streetshares.com>
2016-09-27 06:47:25 -0400

    allow kwargs on resthandler json decorator

--- rhc/resthandler.py
+++ rhc/resthandler.py
@@ -338,7 +338,7 @@ class RESTMapping(object):
         }
 
 
-def content_to_json(*fields):
+def content_to_json(*fields, **kwargs):
     '''rest_handler decorator that converts handler.html_content to handler.json
 
     The content must be a valid json document or a valid URI query string (as
@@ -358,26 +358,45 @@ def content_to_json(*fields):
                  a new value. for instance ('a', int) will look up the value
                  for 'a', and convert it to an int (or fail trying).
 
+                 if field name is a tuple with three elements, then the third
+                 element is a default value.
+        as_args - if true, append fields as described above, else add to decorated
+                  call as kwargs.
+
     Errors:
         400 - json conversion fails or specified fields not present in json
     Notes:
          1. This is responsive to the is_delayed flag on the request.
     '''
+    as_args = kwargs.setdefault('as_args', True)
+
     def __content_to_json(rest_handler):
         def inner(request, *args):
+            kwargs = dict()
             try:
                 if fields:
                     args = list(args)
                     for field in fields:
-                        fname, ftype = field if isinstance(field, tuple) else (field, None)
-                        value = request.json[fname]
-                        if ftype:
-                            value = ftype(value)
-                        args.append(value)
+                        if isinstance(field, tuple):
+                            if len(field) == 3:
+                                fname, ftype, fdflt = field
+                                value = request.json.get(fname, fdflt)
+                            else:
+                                fname, ftype = field
+                                value = request.json[fname]
+                            if ftype:
+                                value = ftype(value)
+                        else:
+                            fname = field
+                            value = request.json[fname]
+                        if as_args:
+                            args.append(value)
+                        else:
+                            kwargs[fname] = value
             except KeyError as e:
                 return request.respond(RESTResult(400, 'Missing required key: %s' % str(e)))
             except Exception as e:
-                return request.respond(RESTResult(400, e.message))
-            return rest_handler(request, *args)
+                return request.respond(RESTResult(400, "Unable to read field '%s': %s" % (fname, e.message)))
+            return rest_handler(request, *args, **kwargs)
         return inner
     return __content_to_json
