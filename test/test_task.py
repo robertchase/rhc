de5c344fb3489bacdfeb49148a055d1e9cd864d5
bob chase <bchase@streetshares.com>
2017-04-17 06:19:15 -0400

    basic task tests

---
+++ test/test_task.py
@@ -0,0 +1,43 @@
+import pytest
+
+import rhc.task as task
+
+
+@pytest.fixture
+def happy():
+    def cb(rc, result):
+        assert rc == 0
+    return task.Task(cb)
+
+
+@pytest.fixture
+def not_happy():
+    def cb(rc, result):
+        assert rc != 0
+    return task.Task(cb)
+
+
+def test_simple(happy):
+    happy.respond('yay')
+
+
+def test_error(not_happy):
+    not_happy.error('boo')
+
+
+def task_cmd(task, result):
+    task.worked = True
+
+
+def partial_happy(cb):
+    cb(0, 'yay')
+
+
+def partial_not_happy(cb):
+    cb(1, 'boo')
+
+
+def test_defer(happy):
+    happy.worked = False
+    happy.defer(task_cmd, partial_happy)
+    assert happy.worked
