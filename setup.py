303d8385fa8c3469a3893b51c04b7be390c35120
bob chase <bchase@streetshares.com>
2017-05-01 14:22:41 -0400

    add micro_fsm directory to setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@ from distutils.core import setup
 setup(
     name='rhc',
     version='2.0',
-    packages=['rhc', 'rhc/database', 'rhc/fsm'],
+    packages=[
+        'rhc',
+        'rhc/database',
+        'rhc/micro_fsm',
+        'rhc/fsm'
+    ],
 )
