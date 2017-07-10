4a30067fb6df48f287cb293d0f934b32dfee0d3b
Andrew Ahlers <aahlers@streetshares.com>
2017-07-10 14:18:38 -0400

    When parsing a micro file, relative paths are relative to that file,
    not the cwd. This seems more natural and removes dependencies on
    the current working directory.

--- rhc/file_util.py
+++ rhc/file_util.py
@@ -2,8 +2,12 @@ import imp
 import os
 
 
-def normalize_path(path, filetype=None):
-    ''' Convert dot-separated paths to directory paths '''
+def normalize_path(path, filetype=None, parent=None):
+    ''' Convert dot-separated paths to directory paths
+
+    Paths are relative to the parent file or the cwd if
+    no parent is available.
+    '''
     if not isinstance(path, str):
         return path
     if '.' in path and os.path.sep not in path:  # path is dot separated
@@ -16,5 +20,9 @@ def normalize_path(path, filetype=None):
             sink, path, sink = imp.find_module(parts[0])  # use module-based location
             path = os.path.join(path, *parts[1:]) + extension
     elif not path.startswith(os.path.sep):  # path is relative
-        path = os.path.join(os.getcwd(), path)
+        if parent:
+            parent_dir = os.path.dirname(parent)
+        else:
+            parent_dir = os.getcwd()
+        path = os.path.join(parent_dir, path)
     return path
