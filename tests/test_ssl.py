cbd10c7426ea4b3ec1e279fb1386022b8e83ad47
Brendan Goggin <bgoggin@streetshares.com>
2017-12-13 15:50:06 -0500

    fixed a test case

--- tests/test_ssl.py
+++ tests/test_ssl.py
@@ -1,5 +1,5 @@
 import rhc.tcpsocket as network
-
+import os
 
 PORT = 12345
 
@@ -46,8 +46,11 @@ class SuccessClient(Client):
 
 def test_success():
     n = network.Server()
+    cert_dir = os.path.dirname(__file__) + '/cert/'
+    certfile = cert_dir + 'cert.pem'
+    keyfile = cert_dir + 'key.pem'
     n.add_server(PORT, network.BasicHandler, ssl=True,
-                 ssl_certfile='cert/cert.pem', ssl_keyfile='cert/key.pem')  # self-signed certs
+                 ssl_certfile=certfile, ssl_keyfile=keyfile)  # self-signed certs
     c = n.add_connection(('localhost', PORT), SuccessClient, ssl=True)
     while c.is_open:
         n.service()
